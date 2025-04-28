7899cf14031b2c6dc87f7cfc5b6bca2c34475239
Feiyang Wu <54_backer_morass@icloud.com>
2025-04-27 21:32:32 -0400

    add more award types

--- core/publications/default-awards.php
+++ core/publications/default-awards.php
@@ -40,4 +40,30 @@ function tp_register_all_awards() {
             'icon'              => 'fas fa-award'
         ) );
 
+    // ICRA Best Paper Award Finalist
+    tp_register_award(
+        array(
+            'award_slug'        => 'icra_best_paper_finalist',
+            'i18n_singular'     => esc_html__('ICRA Best Paper Award Finalist','teachpress'),
+            'i18n_plural'       => esc_html__('ICRA Best Paper Award Finalists','teachpress'),
+            'icon'              => 'fas fa-star'
+        ) );
+
+    // IEEE-RAS TC on Whole-Body Control Best Paper Award Finalist
+    tp_register_award(
+        array(
+            'award_slug'        => 'ieee_ras_tc_best_paper_finalist',
+            'i18n_singular'     => esc_html__('IEEE-RAS TC on Whole-Body Control Best Paper Award Finalist','teachpress'),
+            'i18n_plural'       => esc_html__('IEEE-RAS TC on Whole-Body Control Best Paper Award Finalists','teachpress'),
+            'icon'              => 'fas fa-star'
+        ) );
+
+    // Best Late Breaking Results Poster Award
+    tp_register_award(
+        array(
+            'award_slug'        => 'best_late_breaking_results_poster_award',
+            'i18n_singular'     => esc_html__('Best Late Breaking Results Poster Award','teachpress'),
+            'i18n_plural'       => esc_html__('Best Late Breaking Results Poster Awards','teachpress'),
+            'icon'              => 'fas fa-trophy'
+        ) );
 }
